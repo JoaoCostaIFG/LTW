d6c5e87b65f7c57c2ea6ce0e4b6b1ad1ba932fd8
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-11-05 22:35:50 +0000

    Update: gallery is done (maybe missing cropping feature).

--- pages/home.php
+++ pages/home.php
@@ -6,56 +6,62 @@
   include('../templates/common/tpl_header.php');
 ?>
 
-  <div id="list">
-    <div class="gallery">
-      <a target="_blank" href="/static/tmp/a.png">
-        <img src="/static/tmp/a.png" alt="a.">
-      </a>
-      <div class="desc">a.</div>
-    </div>
-    <div class="gallery">
-      <a target="_blank" href="/static/tmp/b.png">
-        <img src="/static/tmp/b.png" alt="b.">
-      </a>
-      <div class="desc">b.</div>
-    </div>
-    <div class="gallery">
-      <a target="_blank" href="/static/tmp/c.png">
-        <img src="/static/tmp/c.png" alt="c.">
-      </a>
-      <div class="desc">c.</div>
-    </div>
-    <div class="gallery">
-      <a target="_blank" href="/static/tmp/d.png">
-        <img src="/static/tmp/d.png" alt="d.">
-      </a>
-      <div class="desc">d.</div>
-    </div>
-    <div class="gallery">
-      <a target="_blank" href="/static/tmp/e.png">
-        <img src="/static/tmp/e.png" alt="e.">
-      </a>
-      <div class="desc">e.</div>
-    </div>
-    <div class="gallery">
-      <a target="_blank" href="/static/tmp/f.png">
-        <img src="/static/tmp/f.png" alt="f.">
-      </a>
-      <div class="desc">f.</div>
-    </div>
-    <div class="gallery">
-      <a target="_blank" href="/static/tmp/g.png">
-        <img src="/static/tmp/g.png" alt="g.">
-      </a>
-      <div class="desc">g.</div>
-    </div>
-    <div class="gallery">
-      <a target="_blank" href="/static/tmp/h.png">
-        <img src="/static/tmp/h.png" alt="h.">
-      </a>
-      <div class="desc">h.</div>
-    </div>
-  </div>
+<div class="list">
+  <a href="/static/tmp/a.png" class="list-item">
+    <ul class="list-item-content">
+      <li><img src="/static/tmp/a.png" alt="a."/></li>
+      <li>uwu</li>
+    </ul>
+  </a>
+  <a href="/static/tmp/b.png" class="list-item">
+    <ul class="list-item-content">
+      <li><img src="/static/tmp/b.png" alt="b."/></li>
+      <li>Mia</li>
+    </ul>
+  </a>
+  <a href="/static/tmp/c.png" class="list-item">
+    <ul class="list-item-content">
+      <li><img src="/static/tmp/c.png" alt="c."/></li>
+      <li>Gatoplank</li>
+    </ul>
+  </a>
+  <a href="/static/tmp/d.png" class="list-item">
+    <ul class="list-item-content">
+      <li><img src="/static/tmp/d.png" alt="d."/></li>
+      <li>Chico</li>
+    </ul>
+  </a>
+  <a href="/static/tmp/e.png" class="list-item">
+    <ul class="list-item-content">
+      <li><img src="/static/tmp/e.png" alt="e."/></li>
+      <li>Lucas</li>
+    </ul>
+  </a>
+  <a href="/static/tmp/f.png" class="list-item">
+    <ul class="list-item-content">
+      <li><img src="/static/tmp/f.png" alt="f."/></li>
+      <li>Bazinga</li>
+    </ul>
+  </a>
+  <a href="/static/tmp/g.png" class="list-item">
+    <ul class="list-item-content">
+      <li><img src="/static/tmp/g.png" alt="g."/></li>
+      <li>Golden</li>
+    </ul>
+  </a>
+  <a href="/static/tmp/h.png" class="list-item">
+    <ul class="list-item-content">
+      <li><img src="/static/tmp/h.png" alt="h."/></li>
+      <li>Daisy</li>
+    </ul>
+  </a>
+  <a href="/static/tmp/a.png" class="list-item">
+    <ul class="list-item-content">
+      <li><img src="/static/tmp/a.png" alt="a."/></li>
+      <li>uwu</li>
+    </ul>
+  </a>
+</div>
 
 <?php
   include('../templates/common/tpl_footer.php');
