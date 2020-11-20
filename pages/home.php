6f1f8227a9a83d0d5c76f0d85488d87a78621bb9
Fontão <up201806317@fe.up.pt>
2020-11-20 12:23:41 +0000

    Initial homepage

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,14 @@
 <?php
-  header('Location: list.php');
+  include('../templates/common/tpl_header.php');
+?>
+
+<button type="button">Start Searching</button> 
+
+<section id="about">
+    <h2>About us</h2>
+    <p>We are a company that aims to connect animals with their future owners ensuring they are well taken care of.</p>
+    <img id="aboutimg" src="/static/images/bazinga.png" alt="Turtle."/>
+</section>
+<?php
+  include('../templates/common/tpl_footer.php');
 ?>
--- templates/common/tpl_header.php
+++ templates/common/tpl_header.php
@@ -4,9 +4,9 @@
     <title>Become my buddy</title>    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../../css/style.css" rel="stylesheet">
-    <link href="../../css/layout.css" rel="stylesheet">
-    <link href="../../css/responsive.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="../css/layout.css" rel="stylesheet">
+    <link href="../css/responsive.css" rel="stylesheet">
   </head>
   <body>
     <nav id="menu">
