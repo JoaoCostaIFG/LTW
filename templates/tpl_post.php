5fb1c01073ee705ba57f0be35b8d7042a7b9fa06
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-11-27 01:06:45 +0000

    Update: post page has info placed.

--- templates/tpl_post.php
+++ templates/tpl_post.php
@@ -8,21 +8,21 @@
       <b><?php echo $post['name']; ?></b> </br>
       for adoption from <b><?php echo $post['user']; ?></b>
     </h2>
-    <div class="petpost">
-      <ul>
-        <li class="list-item-img">
-        <?php echo '<div style="background: url( ../static/images/' . $post['photo_path'] . ') no-repeat center /auto 100%"></div>'?>
-          <!-- <div style="background: url('<?php echo $post['photo_path']; ?>') no-repeat center /auto 100%"></div> -->
-        </li>
-        <li>Name: <b><?php echo $post['name']; ?></b></li>
-        <li>Age: <b><?php echo $post['age']; ?></b></li>
-        <li>Breed: <b><?php echo $post['species']; ?></b></li>
-        <li>Genre: <b><?php echo $post['gender']; ?></b></li>
-        <li>Size: <b><?php echo $post['size']; ?></b></li>
-        <li>Location: <b><?php echo $post['location']; ?></b></li>
-      </ul>
-      <?php echo $post['date']; ?>
-    </div>
+    <div class="petpost-img" style="background: url(../static/images/<?php echo $post['photo_path']; ?>) no-repeat center /auto 100%"></div>
+    <ul class="petpost">
+      <li>Name: <b><?php echo $post['name']; ?></b></li>
+      <li>Age: <b><?php echo $post['age']; ?></b></li>
+      <li>Breed: <b><?php echo $post['species']; ?></b></li>
+      <li>Genre: <b><?php echo $post['gender']; ?></b></li>
+      <li>Size: <b><?php echo $post['size']; ?></b></li>
+      <li>Location: <b><?php echo $post['location']; ?></b></li>
+    </ul>
+
+    <br>
+    <br>
+    <br>
+
+    <?php echo $post['date']; ?> <br>
     
     <?php echo $post['description']; ?>
 
