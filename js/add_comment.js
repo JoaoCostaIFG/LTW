2bfe125f45fa7dbeb8a17cf601642cebc348e251
ricardofontao2000 <up201806317@fe.up.pt>
2020-12-03 10:31:13 +0000

    Error message

--- js/add_comment.js
+++ js/add_comment.js
@@ -1,8 +1,17 @@
 'use strict'
 
+/*
+Makes a GET request to add a comment and adds that comment if the request is successful
+adds a p with id comment-error if not successful 
+Escapes html
+*/
+
+
 function addComment(post_id, username) {
+    removeError();
     let text = document.getElementsByName("comment_text")[0].value;
     if(text === ""){
+        showError();
         return;
     }
     var xhttp = new XMLHttpRequest();
@@ -12,6 +21,8 @@ function addComment(post_id, username) {
             //Trim is used because the reponse text comes with new lines behind
             if(xhttp.responseText.trim("\n") === "success"){
                 appendComment(text, username);
+            } else {
+                showError();
             }
         }
     };
@@ -38,6 +49,21 @@ function appendComment(text, username) {
     element.appendChild(comment);
 }
 
+function showError(){
+    let comment = document.createElement('p');
+    console.log(comment);
+    comment.setAttribute('id', 'comment-error');
+    comment.innerHTML = "An error ocurred.";
+    let element = document.getElementById("comments");
+    element.appendChild(comment);
+}
+
+function removeError(){
+    let commentError = document.getElementById('comment-error');
+    if(commentError)
+        commentError.remove();
+}
+
 function encodeForAjax(data) {
     return Object.keys(data).map(function(k){
       return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
@@ -55,6 +81,6 @@ let entityMap = {
 
 function escapeHtml(string) {
 return String(string).replace(/[&<>"'\/]/g, function (s) {
-    return entityMap[s];
-});
+        return entityMap[s];
+    });
 }
