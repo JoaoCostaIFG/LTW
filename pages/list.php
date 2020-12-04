558fe91f830782d9caddfd559e09a8b89519bec2
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-12-04 13:35:54 +0000

    Update: css for the listing feature form.

--- pages/list.php
+++ pages/list.php
@@ -12,20 +12,29 @@
 ?>
 
 <form class="listfilter" action="../pages/list.php" method="GET">
-  <br>
-  Name<input type="text" name="name" value="<?php echo $values['name'] ?>">
+  <div class="listfilter-item" >
+    <label for="name">Name</label>
+    <input id="name" type="text" name="name" value="<?php echo $values['name'] ?>">
+  </div>
+
   <?php drawGenders(true, $values['gender']); ?>
-  Age<input type="text" name="age" value="<?php echo $values['age'] ?>">
-  Size<input type="text" name="size" value="<?php echo $values['size'] ?>">
+  <div class="listfilter-item" >
+    <label for="age">Age</label>
+    <input id="age" type="text" name="age" value="<?php echo $values['age'] ?>">
+  </div>
+  <div class="listfilter-item" >
+    <label for="size">Size</label>
+    <input id="age" type="text" name="size" value="<?php echo $values['size'] ?>">
+  </div>
+  
   <?php drawSpecies(true, $values['species']); ?>
   <?php drawCities(true, $values['city']); ?>
 
   <br>
-  <input type="submit" value="Search">
+  <input class="listfilter-button" type="submit" value="Search">
 </form>
 
 <?php 
-
   drawPostList($posts);
   include('../templates/common/tpl_footer.php');
 ?>
