bc462033dc0e0bfa5f70aa70e424c3c295fa9cd6
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-12-06 15:42:26 +0000

    Update: visuals on sent proposal and received proposal pages.

--- templates/tpl_proposal.php
+++ templates/tpl_proposal.php
@@ -1,94 +1,116 @@
 <?php
-include_once '../templates/tpl_utils.php';
+require_once '../templates/tpl_utils.php';
 
-function drawSentProposal($proposal) {
+function drawSentProposal($proposal)
+{
     $post_path = 'post.php?post_id=' . $proposal['post_id'];
     $photo_path = "../static/images/" . $proposal['photo_id'] . "." . $proposal['photo_extension'];
-?>
-    <!-- Proposal from/to -->
-    You made a proposal to
-    <?php echo " " .$proposal['poster_username'] . " ";?>
-    to adopt
-    <?php echo " " . $proposal['pet_name'];?>
 
-    <!-- Pet Image -->
-    <a href="<?php echo $post_path; ?>" class="list-item">
-      <div class="petpost-img">
-        <div style="background: url(<?php echo $photo_path; ?>) no-repeat center /auto 100%"></div>
-      </div>
+    // Proposal from/to
+    $proposal_txt = "You made a proposal to <b>" . $proposal['poster_username'] . "</b> ";
+    $proposal_txt .= "to adopt <b>" . $proposal['pet_name'] . "</b>.";
+
+    // Proposal status
+    $status_txt =  "Your proposal ";
+    if ($proposal['status'] == -1) { 
+        $status_txt .= "<b>is pending";
+    } else if ($proposal['status'] == 0) { 
+        $status_txt .= "<b class='error'>was refused";
+    } else if ($proposal['status'] == 1) { 
+        $status_txt .= "<b class='success'>was accepted";
+    }
+    $status_txt .=  "</b>.";
+
+    // Pet Image
+    ?>
+
+  <div class="petpost">
+    <a class="nounderline petimage petpost-img" href="<?php echo $post_path; ?>">
+      <div style="background: url(<?php echo $photo_path; ?>) no-repeat center /auto 100%"></div>
     </a>
-    <br>
+    <ul class="petpost-info nobullets">
+      <li><?php echo $proposal_txt; ?></li>
+      <li><?php echo $status_txt; ?></li>
+    </ul>
+  </div>
 
-    <!-- Proposal status -->
-    <p>
-        Your proposal
-        <?php
-        if ($proposal['status'] == -1)
-            echo " is pending";
-        else if ($proposal['status'] == 0)
-            echo " was refused";
-        else if ($proposal['status'] == 1)
-            echo " was accepted";
-        ?>
-    </p>
-    <br>
-
-<?php
+    <?php
 }
 
-function drawSentProposals($proposals) {
+function drawSentProposals($proposals)
+{
+    $found_one = false;
     foreach ($proposals as $proposal) {
+        $found_one = true;
         drawSentProposal($proposal);
+    }
+
+    if (!$found_one) {
+        echo "<p>You've made no adoption proposals yet.</p>";
     }
 }
 
-function drawAcceptProposal($user_id, $post_id) { ?>
+function drawAcceptProposal($user_id, $post_id)
+{
+    ?>
         <script src="../js/utils.js" type="text/javascript" defer></script>
         <script src="../js/proposal.js" type="text/javascript" defer></script>
         <button id="acceptButton" onclick="handle_proposal(<?php echo "'accept_proposal', $post_id, $user_id";?>)">
             Accept Proposal</button>
-        <button id="rejectButton" onclick="handle_proposal(<?php echo "'reject_proposal', $post_id, $user_id";?>)">
+        <button id="rejectButton" class="rejbutton" onclick="handle_proposal(<?php echo "'reject_proposal', $post_id, $user_id";?>)">
             Reject Proposal</button>
         <p id="processedButtonText"></p>
-<?php }
-
-
-function  drawReceivedProposal($proposal) {
-    $post_path = 'post.php?post_id=' . $proposal['post_id'];
-    $photo_path = "../static/images/" . $proposal['photo_id'] . "." . $proposal['photo_extension'];
-?>
-    <!-- Proposal from/to -->
-    You received a proposal from
-    <?php echo " " .$proposal['user_username'] . " ";?>
-    to adopt
-    <?php echo " " . $proposal['pet_name'];?>
-
-    <!-- Pet Image -->
-    <a href="<?php echo $post_path; ?>" class="list-item">
-
-      <div class="petpost-img">
-        <div style="background: url(<?php echo $photo_path; ?>) no-repeat center /auto 100%"></div>
-      </div>
-    </a>
-    <br>
-
-    <!-- Proposal status -->
     <?php
-    if ($proposal['status'] == -1)
-        drawAcceptProposal($proposal['user_id'], $proposal['post_id']);
-    else if ($proposal['status'] == 0)
-        echo "You refused this proposal";
-    else if ($proposal['status'] == 1)
-        echo "You accepted this proposal";
-    ?>
-    <br>
-
-<?php
 }
 
-function drawReceivedProposals($proposals) {
+
+function drawReceivedProposal($proposal)
+{
+    $post_path = 'post.php?post_id=' . $proposal['post_id'];
+    $photo_path = "../static/images/" . $proposal['photo_id'] . "." . $proposal['photo_extension'];
+
+    // Proposal from/to
+    $proposal_txt = "You received a proposal from <b>" . $proposal['user_username'] . "</b> ";
+    $proposal_txt .= "to adopt <b>" . $proposal['pet_name'] . "</b>.";
+
+    // Pet Image
+    ?>
+
+    <div class="petpost">
+      <a class="nounderline petimage petpost-img" href="<?php echo $post_path; ?>">
+        <div style="background: url(<?php echo $photo_path; ?>) no-repeat center /auto 100%"></div>
+      </a>
+      <ul class="petpost-info nobullets">
+        <li><?php echo $proposal_txt; ?></li>
+        <li>
+          <?php
+          // Proposal status
+
+          if ($proposal['status'] == -1) {
+              drawAcceptProposal($proposal['user_id'], $proposal['post_id']);
+          } else if ($proposal['status'] == 0) {
+              echo "You refused this proposal.";
+          } else if ($proposal['status'] == 1) {
+              echo "You accepted this proposal.";
+          }
+          ?>
+        </li>
+      </ul>
+    </div>
+
+    <?php
+}
+
+function drawReceivedProposals($proposals)
+{
+    $found_one = false;
     foreach ($proposals as $proposal) {
+        $found_one = true;
         drawReceivedProposal($proposal);
+    }
+
+    if (!$found_one) {
+        echo "<p>You've received no adoption proposals yet.</p>";
     }
 }
 ?>
