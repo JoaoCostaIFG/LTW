ffca31013a9a4f774899ed6b769a7f47114a0659
ricardofontao2000 <up201806317@fe.up.pt>
2020-12-06 21:36:02 +0000

    css done

--- js/confirmation_bar.js
+++ js/confirmation_bar.js
@@ -8,10 +8,11 @@ Creates a confirmation prompt on a scpecific action
 function showConfirmationPrompt(message, callOnSuccess) {
     if(!document.getElementById("confirmationBar")){
         let body = document.getElementsByTagName("body")[0];
+        body.style.marginBottom = getComputedStyle(document.documentElement).getPropertyValue('--bottom-bar-height');
         body.innerHTML += "<section id=\"confirmationBar\">" +
                                 "<p>" + message + "</p>" + 
-                                "<button id=\"acceptConfirmationButton\" onclick=\"" + callOnSuccess + ";removeConfirmationBar()\">Yes</button>" + 
                                 "<button id=\"rejectConfirmationButton\" onclick=\"removeConfirmationBar()\">No</button>" + 
+                                "<button id=\"acceptConfirmationButton\" onclick=\"" + callOnSuccess + ";removeConfirmationBar()\">Yes</button>" + 
                             "</section>";
     }
 }
@@ -20,5 +21,6 @@ function removeConfirmationBar() {
     let prompt = document.getElementById("confirmationBar");
     if(prompt){
         prompt.remove();
+        document.getElementsByTagName("body")[0].style.marginBottom = "initial";
     }
 }
