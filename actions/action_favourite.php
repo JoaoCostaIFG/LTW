40e2c91974bcfd092921ab152702bc40168867f2
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-12-06 21:53:06 +0000

    nice commit fogao

--- actions/action_favourite.php
+++ actions/action_favourite.php
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-<?php
-  include_once('../pages/session.php');
-  include_once('../database/queries/db_user.php');
-  
-  if (!isset($_SESSION['username']) || !isset($_GET['post_id'])){
-    echo 'error';
-    die;
-  }
-  
-  $user_id = getUserId($_SESSION['username'])['id'];
-  $post_id = $_GET['post_id'];
-
-
-  try {
-    if(isFavourite($user_id, $post_id)) {
-        removeFavouritePost($user_id, $post_id);
-        echo 'removed';
-    } else {
-        addFavouritePost($user_id, $post_id);
-        echo 'added';
-    }
-  } catch (PDOException $e) {
-    echo 'error';
-    die;
-  }
-?>
-=======
 <?php
   require_once '../pages/session.php';
   require_once '../database/queries/db_user.php';
@@ -52,4 +24,3 @@ try {
     die;
 }
 ?>
->>>>>>> development
