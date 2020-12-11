d809fc6e1d18a9fd4a916d584d326e88dce49abe
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-12-11 12:38:17 +0000

    Fix: wrong gets on profile + post pages will redirect.

--- pages/post.php
+++ pages/post.php
@@ -6,6 +6,9 @@ if (!$_GET['post_id']) { // go to 404 if there is no post id
 
   require '../database/queries/db_post.php';
   $post = getPost($_GET['post_id']);
+  if (!isset($post['id'])) {
+    die(header('Location: ../pages/list.php'));
+  }
   $title=$post['name'];
   require '../templates/common/tpl_header.php';
 
--- pages/profile.php
+++ pages/profile.php
@@ -1,16 +1,27 @@
 <?php
   require_once '../includes/session.php';
 
-if (!isset($_SESSION['username'])) {
-    die(header('Location: 404.php'));
-} 
+$username="";
+if (isset($_GET['username'])) {
+  $username=$_GET['username'];
+}
+else if (isset($_SESSION['username'])) {
+  // if no username is given, check current signed in user (if any)
+  $username=$_SESSION['username'];
+}
+else {
+  die(header('Location: ../pages/list.php'));
+}
+
+  require '../database/queries/db_user.php';
+  $user_info = getUserInfo($username);
+  if (!isset($user_info['id'])) {
+    die(header('Location: ../pages/list.php'));
+  }
 
   require_once '../templates/common/tpl_header_noimg.php';
-  require '../database/queries/db_user.php';
-  require '../templates/tpl_profile.php';
 
-  $username = $_GET['username'];
-  $user_info = getUserInfo($username);
+  require '../templates/tpl_profile.php';
   $user_posts = getPostsByUser($username);
   
   drawProfile($user_info, $user_posts);
