54ae814a53dda92c450d0b29c5469df7b6d283dc
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-12-13 23:18:35 +0000

    Fix: forgot variable on deploy script.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ printf "User: "
 read -r user
 
 ssh "${user}"@gnomo.fe.up.pt rm -rf public_html/*
-scp -r actions api css database includes js pages static templates favicon.ico index.php up201806560@gnomo.fe.up.pt:public_html/
+scp -r actions api css database includes js pages static templates favicon.ico index.php "${user}"@gnomo.fe.up.pt:public_html/
