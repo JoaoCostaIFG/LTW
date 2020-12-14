35ebb4041a3d84fb487637b14af94725cacbd702
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-12-14 15:47:54 +0000

    Update: set http_response_codes in REST api.

--- api/post.php
+++ api/post.php
@@ -1,91 +1,107 @@
 <?php
-    require_once '../includes/session.php';
-    require_once '../database/queries/db_user.php';
-    require_once '../database/queries/db_post.php';
-    require_once '../actions/action_upload.php';
-    require_once '../includes/utils.php';
+require_once '../includes/session.php';
+require_once '../includes/utils.php';
+require_once '../actions/action_upload.php';
+require_once '../database/queries/db_user.php';
+require_once '../database/queries/db_post.php';
 
-  if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     header('Content-Type: application/json');
     if (!isset($_GET['id'])) {
-      $posts = getPosts(array(), array(), "");
-      if(!isset($posts)){
-          die(json_encode(array('error' => 'posts_not_found')));
-      }
-      echo json_encode($posts);
+        $posts = getPosts(array(), array(), "");
+        if(!isset($posts)) {
+            http_response_code(204);
+            echo json_encode(array('message' => 'No content'));
+        }
+        else {
+            http_response_code(200);
+            echo json_encode($posts);
+        }
     } else {
-      $post_id = $_GET['id'];
-      $post = getPost($post_id);
-      if(!isset($post)){
-          die(json_encode(array('error' => 'post_not_found')));
-      }
-      echo json_encode($post);
+        $post_id = $_GET['id'];
+        $post = getPost($post_id);
+        if(!isset($post)) {
+            http_response_code(404);
+            echo json_encode(array('message' => 'Post not found'));
+        }
+        http_response_code(200);
+        echo json_encode($post);
     }
-  } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-    if(!isset($_SESSION['username'])){
-        echo 'Request failed: Not authenticated';
-        die();
+} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(!isset($_SESSION['username'])) {
+        http_response_code(401);
+        echo json_encode(array('message' => 'Request failed: Not authenticated'));
+        die;
     }
 
     $type = photoIsValid($_FILES['image']['tmp_name']);
     if (!$type) {
-        echo 'Request failed';
-        die();
+        http_response_code(400);
+        echo json_encode(array('message' => 'Request failed: photo is not defined/invalid'));
+        die;
     }
-    if(!isset($_POST['name'])){
-        echo 'Request failed: name is not defined';
-        die();
+    $name = treatInputNonEmpty($_POST['name']);
+    if(!isset($name)) {
+        http_response_code(400);
+        echo json_encode(array('message' => 'Request failed: name is not defined/invalid'));
+        die;
     }
-    if(!isset($_POST['birth_date'])){
-        echo 'Request failed: birth_date is not defined';
-        die();
+    $birth_date = treatInputNonEmpty($_POST['birth_date']);
+    if(!isset($birth_date)) {
+        http_response_code(400);
+        echo json_encode(array('message' => 'Request failed: birth_date is not defined/invalid'));
+        die;
     }
-    if(!isset($_POST['gender'])){
-        echo 'Request failed: gender is not defined';
-        die();
+    $gender = $_POST['gender'];
+    if(!isset($gender)) {
+        http_response_code(400);
+        echo json_encode(array('message' => 'Request failed: gender is not defined/invalid'));
+        die;
     }
-    if(!isset($_POST['size'])){
-        echo 'Request failed: size is not defined';
-        die();
+    $size = $_POST['size'];
+    if(!isset($size)) {
+        http_response_code(400);
+        echo json_encode(array('message' => 'Request failed: size is not defined/invalid'));
+        die;
     }
-    if(!isset($_POST['description'])){
-        echo 'Request failed: description is not defined';
-        die();
+    $description = $_POST['description'];
+    if(!isset($description)) {
+        http_response_code(400);
+        echo json_encode(array('message' => 'Request failed: description is not defined/invalid'));
+        die;
     }
-    if(!isset($_POST['color'])){
-        echo 'Request failed: color is not defined';
-        die();
+    $color = $_POST['color'];
+    if(!isset($color)) {
+        http_response_code(400);
+        echo json_encode(array('message' => 'Request failed: color is not defined/invalid'));
+        die;
     }
-    if(!isset($_POST['species'])){
-        echo 'Request failed: species is not defined';
-        die();
+    $species = $_POST['species'];
+    if(!isset($species)) {
+        http_response_code(400);
+        echo json_encode(array('message' => 'Request failed: species is not defined/invalid'));
+        die;
     }
-    if(!isset($_POST['city'])){
-        echo 'Request failed: city is not defined';
-        die();
+    $city = $_POST['city'];
+    if(!isset($city)) {
+        http_response_code(400);
+        echo json_encode(array('message' => 'Request failed: city is not defined/invalid'));
+        die;
     }
 
-    $name = $_POST['name'];
-    $birth_date = $_POST['birth_date'];
-    $gender = $_POST['gender'];
-    $size = $_POST['size'];
-    $description = $_POST['description'];
-    $color = $_POST['color'];
-    $species = $_POST['species'];
-    $city = $_POST['city'];
     $user_id = getUserId($_SESSION['username'])['id'];
     $post_info = array($name, $birth_date, $gender, $size, $description, $color,
-        $species, $city, $user_id);
+      $species, $city, $user_id);
     
     try {
         $post_id = insertPost($post_info);
         uploadPhoto($post_id, $type, false);
-
-        echo 'Request successful';
+        http_response_code(204);
+        echo json_encode(array('message' => 'Success: post created.'));
     } catch (Exception $e) {
-        echo 'Request failed';
+        http_response_code(503);
+        echo json_encode(array('message' => 'Request failed: Request was OK but server failed.'));
     }
-  }
+}
 
 ?>
