7ea20de274b343b59e7f2c096c0db5389267a41c
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-12-14 22:42:14 +0000

    Fix: confirmation bar css responsiveness.

--- js/confirmation_bar.js
+++ js/confirmation_bar.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 Creates a confirmation prompt on a scpecific action
@@ -6,21 +6,30 @@ Creates a confirmation prompt on a scpecific action
 */
 
 function showConfirmationPrompt(message, callOnSuccess) {
-    if(!document.getElementById("confirmationBar")){
-        let body = document.getElementsByTagName("body")[0];
-        body.style.marginBottom = getComputedStyle(document.documentElement).getPropertyValue('--bottom-bar-height');
-        body.innerHTML += "<section id=\"confirmationBar\">" +
-                                "<p>" + message + "</p>" + 
-                                "<button id=\"rejectConfirmationButton\" onclick=\"removeConfirmationBar()\">No</button>" + 
-                                "<button id=\"acceptConfirmationButton\" onclick=\"" + callOnSuccess + ";removeConfirmationBar()\">Yes</button>" + 
-                            "</section>";
-    }
+  if (!document.getElementById("confirmationBar")) {
+    let body = document.getElementsByTagName("body")[0];
+    body.style.marginBottom = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue("--bottom-bar-height");
+    body.innerHTML +=
+      '<section id="confirmationBar">' +
+      "<p>" +
+      message +
+      "</p>" +
+      '<div id="confirmationButtons">' +
+      '<button id="rejectConfirmationButton" onclick="removeConfirmationBar()">No</button>' +
+      '<button id="acceptConfirmationButton" onclick="' +
+      callOnSuccess +
+      ';removeConfirmationBar()">Yes</button>' +
+      "</div>" +
+      "</section>";
+  }
 }
 
 function removeConfirmationBar() {
-    let prompt = document.getElementById("confirmationBar");
-    if(prompt){
-        prompt.remove();
-        document.getElementsByTagName("body")[0].style.marginBottom = "initial";
-    }
+  let prompt = document.getElementById("confirmationBar");
+  if (prompt) {
+    prompt.remove();
+    document.getElementsByTagName("body")[0].style.marginBottom = "initial";
+  }
 }
